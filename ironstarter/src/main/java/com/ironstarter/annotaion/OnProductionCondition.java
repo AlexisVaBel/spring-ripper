ab2a4e0a8785d11d3fe9d3e5fac31bb4849bd68e
alexisvabel <alexisvabel@gmail.com>
2018-10-18 09:34:46 +0700

    Merge

---
+++ ironstarter/src/main/java/com/ironstarter/annotaion/OnProductionCondition.java
@@ -0,0 +1,14 @@
+package com.ironstarter.annotaion;
+
+import org.springframework.context.annotation.Condition;
+import org.springframework.context.annotation.ConditionContext;
+import org.springframework.core.type.AnnotatedTypeMetadata;
+
+import javax.swing.*;
+
+public class OnProductionCondition implements Condition {
+    @Override
+    public boolean matches(ConditionContext conditionContext, AnnotatedTypeMetadata annotatedTypeMetadata) {
+        return true;//JOptionPane.showConfirmDialog(null,"production?") == 0;
+    }
+}
